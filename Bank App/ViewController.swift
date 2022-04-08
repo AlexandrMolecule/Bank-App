60d6fcbc76039822bb01a4afecadf82522aaebf4
Aleksandr <sanyamolekula@gmail.com>
2022-04-08 11:44:56 +0300

    Initial Commit

---
+++ Bank App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank App
+//
+//  Created by Alexandr Gerasimov on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
